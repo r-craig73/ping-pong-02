f4cb9df9f0b5ceebf0a67e2dee95f25e81cf58ac
Anna-Marie Cooper <annamarie@rogueminx.com>
2017-11-11 18:55:29 -0800

    add javascript to push pingpong acurately into array.

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,12 @@ var countedNumbers = [];
 var counter = function(number) {
   for (var index = 1; index <= number; index += 1) {
   // console.log("The current number is: " + index );
-  if (index % 3 === 0){
+  if (index % 3 === 0 && index % 5 === 0){
+    countedNumbers.push("pingpong");
+  } else if (index % 3 === 0){
     countedNumbers.push("ping");
   } else if (index % 5 === 0){
-    countedNumbers.push("pong");
+  countedNumbers.push("pong");
   } else {
     countedNumbers.push(index);
   }
