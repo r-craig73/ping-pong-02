4d83d1bb6c587f1ddeba5fa8dda8ff4be8891454
Anna-Marie Cooper <annamarie@rogueminx.com>
2017-11-11 20:44:01 -0800

    add javascript to get answers to populate in ul, without being repetitive.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,19 @@
 //back end logic
-var countedNumbers = [];
 var counter = function(number) {
+  var countedNumbers = [];
   for (var index = 1; index <= number; index += 1) {
-  // console.log("The current number is: " + index );
   if (index % 3 === 0 && index % 5 === 0){
-    countedNumbers.push("pingpong");
+    countedNumbers.push("PING PONG!");
   } else if (index % 3 === 0){
     countedNumbers.push("ping");
   } else if (index % 5 === 0){
-  countedNumbers.push("pong");
+    countedNumbers.push("pong");
   } else {
     countedNumbers.push(index);
   }
 }
-console.log(countedNumbers);
-return countedNumbers;
+console.log(countedNumbers.join(" "));
+return (countedNumbers);
 }
 
 
@@ -22,8 +21,12 @@ return countedNumbers;
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
+    $(".unstyled").empty();
     var number = parseInt($("#number").val());
-    var result = counter(number);
-    $("#result").text(result);
+    var results = counter(number);
+    results.forEach(function(result){
+      $(".unstyled").append("<li>" + result + "</li>")
+    })
+    $("#result").show();
   });
 });
