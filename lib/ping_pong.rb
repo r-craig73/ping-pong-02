df4c67c92769ed23f457ec74f36116b61367badd
Ron Craig <ron.craig@comcast.net>
2018-09-12 21:29:47 -0700

    edit and clean up code

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,18 +1,16 @@
-
-
 def ping_pong(number)
   new_array = []
   x = 0
-  while (x < number)
-    x = x + 1
-    if (x.%(3).eql?(0)) & (x.%(5).eql?(0))
-      new_array.push("PINGPONG")
-    elsif x.%(3).eql?(0)
-      new_array.push("ping")
-    elsif x.%(5).eql?(0)
-      new_array.push("pong")
+  while x < number
+    x += 1
+    if (x.%(3).eql? 0) & (x.%(5).eql? 0)
+      new_array.push('PINGPONG')
+    elsif x.%(3).eql? 0
+      new_array.push('ping')
+    elsif x.%(5).eql? 0
+      new_array.push('pong')
     else
-    new_array.push(x)
+      new_array.push(x)
     end
   end
   new_array
